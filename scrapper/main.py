0bdba5047ed1b62aefe6770c8f64deb61fcf2abf
prestonjcrowe <prestonjcrowe@gmail.com>
2020-03-05 13:15:34 -0800

    suboptimal bsoup solution

--- scrapper/main.py
+++ scrapper/main.py
@@ -5,15 +5,75 @@ from urllib.request import Request, urlopen
 
 
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36'
-BASE_URL = 'https://oldschool.runescape.wiki/w/Module:Exchange/{}/Data'
+BASE_URL = 'https://oldschool.runescape.wiki/w/Module:Exchange/{}'
+SUMMARY_URL = 'https://rsbuddy.com/exchange/summary.json'
 ITEMS = './better_items.json'
 PRICE_FILE_DEST = './item_prices.json'
 
 # Dictionary of item names to a corrected version
 ITEM_NAME_CORRECTIONS = {}
 
+def getMetaData(item_name):
+  item_url = BASE_URL.format(item_name)
+  req = Request(item_url, headers={ 'User-Agent': USER_AGENT })
+  try:
+    res = urlopen(req)
+  except:
+    print('FAILED!')
+  
+  soup = BeautifulSoup(res.read(), 'html.parser')
+  pre = soup.find('pre')
+  # remove 'return' keyword from front, split on newlines, remove brackets
+  json_text = pre.text[7:].split('\n')[1:-1]
+  for line in json_text:
+    parts = line.split('=')
+    key = parts[0].strip()
+    val = parts[1].strip()
+    # remove trailing commas....
+    if (val[-1] == ','):
+      val = val[:-1]
+    
+    # remove quotes if string...
+    if (val[0] == '\''):
+      val = val[1:-1]
+    
+    # booleans...
+    if (val == 'true'):
+      val = True
+    print(val)
+
+  #json_text = pre.text[7:]
+  print(json_text)
+
+  #blob = json.loads(pre.text)
+  #print(blob)
+  return None
+
+    
+def updateItemMetadata():
+  req = Request( SUMMARY_URL, headers={ 'User-Agent': USER_AGENT })
+  items_to_update = []
+
+  print('Fetching item metadata...')
+  try:
+    res = urlopen(req)
+    blob = json.loads(res.read())
+  except:
+    print('FAILED!')
+    
+  for id in blob:
+    item = blob[id]
+    name = item['name'].replace(' ', '_')
+    # if not in db already...
+    #   add to list of items to be fetched
+    items_to_update.append(name)
+
+  for item_name in items_to_update:
+    metadata = getMetaData(item_name)
+
 
 def main():
+  updateItemMetadata()
   # Get all items
   with open(ITEMS, 'r') as items_file:
     data = json.load(items_file)
@@ -22,10 +82,11 @@ def main():
   price_history = {}
   failed_items = []
   for i, item_name in enumerate(data):
-    if item_name in ITEM_NAME_CORRECTIONS:
-      item_name = ITEM_NAME_CORRECTIONS[item_name]
+    item_name = data[item_name]['name']
+    #item_name = ITEM_NAME_CORRECTIONS[item_name]
     print('Loading ({:04d}/{:4d}): {:.<30s}'.format(i, len(data), item_name), end='')
-    item_url = BASE_URL.format(item_name)
+    item_url = BASE_URL.format(item_name) + '/Data'
+    print(item_url)
     req = Request( item_url, headers={ 'User-Agent': USER_AGENT })
     try:
       res = urlopen(req)
@@ -64,7 +125,6 @@ def main():
   # Write price history to file
   with open(PRICE_FILE_DEST, 'w') as price_file:
     json.dump(price_history, price_file)
-
 
 if __name__ == '__main__':
   main()
